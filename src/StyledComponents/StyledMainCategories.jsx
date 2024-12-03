4faf65ac0186ef3a0721a799be75843253e21675
mariakatt <mariahkatt@gmail.com>
2024-12-03 21:00:20 +0100

    styles

--- src/StyledComponents/StyledMainCategories.jsx
+++ src/StyledComponents/StyledMainCategories.jsx
@@ -15,7 +15,6 @@ const CategoryContainer  = styled.div `
 
     @media screen and (max-width: 480px) {
 
-    .categoryContainer {
         display: grid;    
         grid-template-columns: repeat(2, 1fr); 
         grid-template-rows: auto; 
@@ -26,43 +25,33 @@ const CategoryContainer  = styled.div `
         height: auto;
         margin: 10px 0 10px 0;
         gap: var(--gap-m);
+   
     }
-}
 
-@media screen and (min-width: 481px) and (max-width: 768px){
-
-    .categoryContainer {
-        width: 90%;
-        height: auto;
-        display: grid;    
-        grid-template-columns: repeat(2, 1fr); 
-        grid-template-rows: auto; 
-        margin: var(--margin-m) 0;
+    @media screen and (min-width: 481px) and (max-width: 768px){
+            width: 90%;
+            height: auto;
+            display: grid;    
+            grid-template-columns: repeat(2, 1fr); 
+            grid-template-rows: auto; 
+            margin: var(--margin-m) 0; 
     }
-}
 
-@media screen and (min-width: 769px) and (max-width: 1023px){
-    .categoryContainer {
-        width: 95%;  
-        height: auto;
-        margin: var(--margin-m) 0;
+    @media screen and (min-width: 769px) and (max-width: 1023px){
+            width: 95%;  
+            height: auto;
+            margin: var(--margin-m) 0;
     }
-}
 
-@media screen and (min-width: 1024px) and (max-width: 1443px){
-
-    .categoryContainer {
-        width:80%;
-        height: auto;
-        margin: var(--margin-m) 0;
+    @media screen and (min-width: 1024px) and (max-width: 1443px){
+            width:80%;
+            height: auto;
+            margin: var(--margin-m) 0; 
     }
-}
 
-@media screen and (min-width: 1444px) and (max-width: 1919px) {
-    .categoryContainer {
-        width:60%;
+    @media screen and (min-width: 1444px) and (max-width: 1919px) {
+            width:60%;
     }
-}
 `
 
 export default { CategoryContainer }
